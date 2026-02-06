1900e49a981dd00e5bba1b01f038a7dce0926059
mosodi007 <martins.osodi@gmail.com>
2026-02-06 17:53:01 +0100

    Updated HowItWorksPage.tsx

--- src/pages/HowItWorksPage.tsx
+++ src/pages/HowItWorksPage.tsx
@@ -355,7 +355,7 @@ export function HowItWorksPage() {
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <button
-              onClick={() => navigate('/signup')}
+              onClick={() => navigate('/')}
               className="px-8 py-4 bg-white hover:bg-gray-100 text-black font-medium rounded-lg transition-all"
             >
               Get Started Free
