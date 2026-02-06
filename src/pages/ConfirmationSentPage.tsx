d3ed89f4790fb4ef87e9456aff0b35b5928ea152
mosodi007 <martins.osodi@gmail.com>
2026-02-06 13:07:01 +0100

    Updated ConfirmationSentPage.tsx

--- src/pages/ConfirmationSentPage.tsx
+++ src/pages/ConfirmationSentPage.tsx
@@ -20,7 +20,7 @@ export default function ConfirmationSentPage() {
           <div className="flex justify-center">
             <div className="relative">
               <div className="absolute inset-0 bg-[#000] rounded-full blur-lg opacity-50 animate-pulse"></div>
-              <div className="relative bg-gradient-to-r from-blue-500 to-purple-600 rounded-full p-4">
+              <div className="relative bg-[#000] rounded-full p-4">
                 <Mail className="w-12 h-12 text-white" />
               </div>
             </div>
